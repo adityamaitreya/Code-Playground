aaac15c1d227a1e4c3f3529b23ac8d86555af83f
Aditya Maitreya <adityamaitreya10@gmail.com>
2024-04-13 21:52:19 +0530

    sj

---
+++ C/aaa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  int a, b;
+
+  scanf("%d", &a);
+  scanf("%d", &b);
+  
+  int sum=a+b;
+  int diff=a-b;
+  printf("Sum is: %d\n", sum);
+  printf("Difference is: %d", diff);
