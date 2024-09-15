da6a4301069e196d8bc6ced624b9041afc97288c
A-maitreya <149099766+A-maitreya@users.noreply.github.com>
2024-09-15 14:29:46 +0530

    15-9-24

---
+++ Python/loops.py
@@ -0,0 +1,37 @@
+# list=[1,4,9,16,25,36,49,64,81,100]
+
+# for i in list:
+#     print(i)
+    
+
+# x=int(input("Enter num: "))
+# idx=0
+# tup=(1,4,9,16,25,36,49,64,81,100)
+# for i in tup:
+#     if(x == i):
+#         idx += 1
+#         print(f"{x} founded at {idx}")
+#         break
+# else:
+#     print(f"{x}not founded")
+
+
+#sum of number
+# n=int(input("Enter no: "))
+# sum=0
+# i=0
+# while i<=n:
+#     sum += i
+#     i += 1
+#  print(f"Total sum: {sum}"")
+
+
+# fac of number
+# n=int(input("Enter no:"))
+# fac=1
+# for i in range(1,n+1):
+#     fac *= i
+#     i += 1
+# print(f"Factorial is {fac}")
+
+
